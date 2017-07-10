7853c6622a2ca34500d1931c2bf3c6cda3acbc10
chandrakanth <chandrakanththadakapally@gmail.com>
2017-07-10 10:39:28 -0500

    Generic Delegates

---
+++ GenericDelegates.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Advanced_C_
+{
+    class GenericDelgates
+    {
+        public void ActionMethod(String S)
+        {
+            System.Console.WriteLine("It don't has return Type: "+S);
+        }
+        public int Addition(int a, int b){
+            return a+b;
+        }
+
+        public bool condtion(String s){
+            if(s=="Chandu"){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+}
