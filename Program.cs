2d3a17b37ed9e23a2a0e1477328a924205e46a55
chandrakanth <chandrakanththadakapally@gmail.com>
2017-07-10 10:49:07 -0500

    Do Changes

--- Program.cs
+++ Program.cs
@@ -6,16 +6,7 @@ namespace Advanced_C_
     {
         static void Main(string[] args)
         {
-            GenericDelgates genericDelgates = new GenericDelgates();
-            //Generic Delegate which return type is VOID
-            Action<String> action = new Action<String>(genericDelgates.ActionMethod);
-            action("Chandu");
-            //Generic Delegate which has return type
-            Func<int , int , int> func = new Func<int , int , int>(genericDelgates.Addition); 
-            System.Console.WriteLine(func(10,20));
-            //Generic Delegate which return type is Boolean
-            Predicate<String> predicate = new Predicate<String>(genericDelgates.condtion);
-            System.Console.WriteLine(predicate("Chandu"));
+           
         }
     }
 }
